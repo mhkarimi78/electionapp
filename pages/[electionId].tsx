2accc2f8b9e1a4f7f4335defb5aefe8a1a42f286
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-10-26 09:43:07 +0330

    ,

--- pages/[electionId].tsx
+++ pages/[electionId].tsx
@@ -36,78 +36,77 @@ const ElectionDetail: NextPage = () => {
   }, [election]);
 
   return (
-    <div className="bg-slate-300 h-screen">
-      <div className=" flex justify-around p-20">
-        <div className="text-black flex flex-col items-center">
-          <p className="my-10">ElectionId {Number(id)}</p>
-          {isElectionLoading ? (
-            "loading Election Details"
-          ) : (
-            <div className="flex flex-col items-center">
-              <p>Election Id: {election?.id._hex}</p>
-              <p className="my-10">Election Name: {election?.name}</p>
-              <p>Election Start Date: {startDate}</p>
-              <p className="my-10">Election End Date: {endDate}</p>
-              <p>
-                Election MinPercentage to Win: {Number(election?.minPercentage._hex)}
-              </p>
-              <p className="my-10">
-                Election Number of Winners: {election?.numWinners._hex}
-              </p>
+    <div className="bg-slate-300 h-screen flex justify-around p-20">
+      <div className="text-black flex flex-col items-center">
+        <p className="my-10">ElectionId {Number(id)}</p>
+        {isElectionLoading ? (
+          "loading Election Details"
+        ) : (
+          <div className="flex flex-col items-center">
+            <p>Election Id: {election?.id._hex}</p>
+            <p className="my-10">Election Name: {election?.name}</p>
+            <p>Election Start Date: {startDate}</p>
+            <p className="my-10">Election End Date: {endDate}</p>
+            <p>
+              Election MinPercentage to Win:{" "}
+              {Number(election?.minPercentage._hex)}
+            </p>
+            <p className="my-10">
+              Election Number of Winners: {election?.numWinners._hex}
+            </p>
 
-              {isParticipant ? (
-                <Web3Button
-                  contractAddress={CONTRACT_ADDRESS}
-                  action={(contract) => {
-                    contract.call("vote", [id, address]);
-                  }}
-                >
-                  Vote
-                </Web3Button>
-              ) : (
-                <div className="flex flex-col ">
-                  <div className="mb-4">
-                    <label
-                      className="block text-gray-700 text-sm font-bold mb-2"
-                      htmlFor="name"
-                    >
-                      Enter your name:
-                    </label>
-                    <input
-                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      id="name"
-                      type="text"
-                      placeholder="Enter you name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </div>
-                  <Web3Button
-                    contractAddress={CONTRACT_ADDRESS}
-                    action={(contract) => {
-                      contract.call("joinAsParticipant", [name, address, id]);
-                    }}
+            {isParticipant ? (
+              <Web3Button
+                contractAddress={CONTRACT_ADDRESS}
+                action={(contract) => {
+                  contract.call("vote", [id, address]);
+                }}
+              >
+                Vote
+              </Web3Button>
+            ) : (
+              <div className="flex flex-col ">
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="name"
                   >
-                    join Election
-                  </Web3Button>
+                    Enter your name:
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="Enter you name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
-              )}
-              <div className="my-10">
                 <Web3Button
                   contractAddress={CONTRACT_ADDRESS}
                   action={(contract) => {
-                    contract.call("getWinners", [id]);
+                    contract.call("joinAsParticipant", [name, address, id]);
                   }}
                 >
-                  Calculate Winner
+                  join Election
                 </Web3Button>
               </div>
+            )}
+            <div className="my-10">
+              <Web3Button
+                contractAddress={CONTRACT_ADDRESS}
+                action={(contract) => {
+                  contract.call("getWinners", [id]);
+                }}
+              >
+                Calculate Winner
+              </Web3Button>
             </div>
-          )}
-        </div>
-        <ClubForm />
-        <EditFrom electionId={election?.id._hex} />
+          </div>
+        )}
       </div>
+      <ClubForm />
+      <EditFrom electionId={election?.id._hex} />
     </div>
   );
 };
