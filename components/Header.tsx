11550f6fe337a54426f3f29561015a35e08082fc
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-10-26 09:23:07 +0330

    ,

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,28 +4,12 @@ import { ConnectWallet, Web3Button, useAddress } from "@thirdweb-dev/react";
 import { CONTRACT_ADDRESS } from "../constants/addresses";
 import Link from "next/link";
 
-interface HeaderProps {
-  handleSidebarToggle?: () => void;
-}
-
-const Header: React.FC<HeaderProps> = ({ handleSidebarToggle = undefined }) => {
+const Header = () => {
   const address = useAddress();
 
   return (
     <nav className=" bg-green-950 border-gray-200 border-b-2 w-screen">
       <div className=" flex flex-wrap items-center justify-between  p-4">
-        {handleSidebarToggle && (
-          <button
-            className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
-            onClick={handleSidebarToggle}
-          >
-            <span className="sr-only text-black">Open sidebar</span>
-            <div className="flex items-center text-black ">
-              <IoMenu className="text-xl mr-2" />
-              Menu
-            </div>
-          </button>
-        )}
         <div className="flex items-center self-center text-center justify-center">
           <IoLogoEdge className="self-center text-3xl text-blue-500 mr-2" />
           <span className="self-center text-3xl font-semibold whitespace-nowrap">
